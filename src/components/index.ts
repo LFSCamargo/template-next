567129527f736997398be8b8698a32e0cd825d3a
LFSCamargo <luizepauloxd@gmail.com>
2021-02-19 00:27:10 -0300

    feat: adds the right way to handle routes on next js

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Button';
 export * from './Helmet';
+export * from './Loading';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
-import Home from './home';
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
+import { Loading } from '~/components';
+import { Wrapper } from './styles';
 
 export default function Main() {
-	return <Home />;
+	const { push } = useRouter();
+
+	useEffect(() => {
+		push('home');
+	}, []);
+
+	return (
+		<Wrapper>
+			<Loading />
+		</Wrapper>
+	);
 }
